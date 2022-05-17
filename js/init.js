4d19c713b4b99bb63e2690f2fbbff5e39209194b
Gabriel Amorim <mrgabamo@gmail.com>
2022-05-16 21:29:14 -0300

    Update init.js

--- js/init.js
+++ js/init.js
@@ -125,7 +125,7 @@ var penelope = function() {
 	
 	/*Typed js start*/
 	$("#typed").typed({
-		strings: ['Hey! Im Gabriel, <br/>Senior Product Manager <br/> at Mercado Bitcoin.', 'Olá! Sou Gabriel, <br/>Senior Product Manager <br/> no Mercado Bitcoin.', 'Ciao! Sono Gabriel, <br/>Senior Product Manager <br/> presso Mercado Bitcoin.'],
+		strings: ['Hey! Im Gabriel, <br/>Lead Product Manager <br/> at Mercado Bitcoin.', 'Olá! Sou Gabriel, <br/>Lead Product Manager <br/> no Mercado Bitcoin.', 'Ciao! Sono Gabriel, <br/>Lead Product Manager <br/> presso Mercado Bitcoin.'],
 		typeSpeed: 30,
 		backDelay: 750,
 		loop: true,
